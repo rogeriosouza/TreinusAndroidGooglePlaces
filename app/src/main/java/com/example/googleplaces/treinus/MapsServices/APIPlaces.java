65779e53356346385acc7343febc7630ff4c9030
rogeriosouza <rogerio.fsouza2@gmail.com>
2018-05-03 23:08:46 -0300

    treinus_v3

--- app/src/main/java/com/example/googleplaces/treinus/MapsServices/APIPlaces.java
+++ app/src/main/java/com/example/googleplaces/treinus/MapsServices/APIPlaces.java
@@ -12,7 +12,7 @@ public class APIPlaces {
 
     private static Retrofit retrofit = null;
 
-    public static final String GOOGLE_PLACE_API_KEY = "SEM API";
+    public static final String GOOGLE_PLACE_API_KEY = "SEM API"; 
 
     public static String base_url = "https://maps.googleapis.com/maps/api/";
 
